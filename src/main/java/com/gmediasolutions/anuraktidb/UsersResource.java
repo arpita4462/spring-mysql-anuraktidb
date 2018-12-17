005b5f74f8251e83dffb2da0c5324e097cdb137f
harishkumar <macbook>
2018-12-17 12:32:20 +0530

    initial commit

--- src/main/java/com/gmediasolutions/anuraktidb/UsersResource.java
+++ src/main/java/com/gmediasolutions/anuraktidb/UsersResource.java
@@ -12,16 +12,16 @@ public class UsersResource {
     @Autowired
     UsersRepository usersRepository;
 
+//  Get method
     @GetMapping(value = "/all")
     public List<Users> getAll() {
         return usersRepository.findAll();
     }
-
+//	post method
     @PostMapping(value = "/createUser")
     public List<Users> persist(@RequestBody final Users users) {
         usersRepository.save(users);
         return usersRepository.findAll();
     }
-
 }
 
